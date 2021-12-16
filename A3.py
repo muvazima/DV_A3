dfe6b01ec2a00a3a338b2e31520935f5dee18d80
muvazima <muvazima99@gmail.com>
2021-12-16 09:49:31 +0000

    added legend

--- A3.py
+++ A3.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 from streamlit_folium import folium_static
 import folium
-#import pathlib
+import branca
+from PIL import Image
+image = Image.open('state_legend_white.png')
 from folium import plugins
 hide_streamlit_style = """
             <style>
@@ -23,11 +25,11 @@ def load_csv():
 
 def circle_color(battery):
     if(battery<=20):
-        return 'red'
+        return '#3D3A00'
     elif(battery>20 and battery<=60):
-        return 'orange'
+        return '#0029E0'
     else:
-        return 'green'
+        return '#004700'
 
 def main():
     df=load_csv()
@@ -36,11 +38,18 @@ def main():
     radius=st.sidebar.text_input('Enter radius in m')
     option = st.sidebar.radio('Select Bike Type',list(df['BikeTypeName'].unique()))
 
+
     m = folium.Map(location=[53.350140, -6.266155], zoom_start=12)
-#folium.TileLayer('cartodbpositron').add_to(m)
+    #folium.TileLayer('cartodbpositron').add_to(m)
     colordict = {1: 'red', 2: 'blue', 3: 'black', 4: 'purple', 5:'lightgray'}
     bike_state_dict={1:'Warning - is in move and not rented',2:'Normal',3:'Switched Off',4:'Firmware Upgrade',5:'Laying on the ground'}
+    colormap = branca.colormap.LinearColormap(['red', 'yellow', 'green'])
+    colormap = colormap.to_step(index=[0,20,40,60,80,100])
+    colormap.caption = 'Battery'
+    colormap.add_to(m)
+    #FloatImage(image_file, bottom=0, left=86).add_to(m)
     marker_cluster = plugins.MarkerCluster().add_to(m)
+    
     if(option!=''):
         bike_type_df=df[df.BikeTypeName==option]
         for lat, lon, battery,bike_id, bike_type, bike_state in zip(bike_type_df['Latitude'], bike_type_df['Longitude'], bike_type_df['Battery'],bike_type_df['BikeID'], bike_type_df['BikeTypeName'], bike_type_df['EBikeStateID']):
@@ -48,10 +57,12 @@ def main():
                     'Bike ID: ' + str(bike_id) + '<br>'
                     'Bike State: ' + bike_state_dict[bike_state]
                     ), icon=folium.Icon(color=colordict[bike_state],icon='bicycle', prefix='fa')).add_to(marker_cluster)
+            
             if(radius!=''):
-                folium.Circle(location=[lat, lon], radius=int(radius), fill=True, fill_color=circle_color(battery),weight=0,opacity=0.001*battery).add_to(m)
-
+                folium.Circle(location=[lat, lon], radius=int(radius), fill=True, fill_color=colormap(battery),weight=0,opacity=0.001*battery).add_to(m)
+    
     folium_static(m)
+    st.image(image, caption='Bike State',use_column_width=True)
 
 if __name__== "__main__":
     main()
